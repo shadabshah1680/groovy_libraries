683d0fd20761bc42174eeb222f07241272fb232a
Shadab Shah <shahshadab1680@gmail.com>
2022-08-30 01:12:09 +0500

    slack_alert

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,16 +1,25 @@
-#!/usr/bin/env groovy
+def notifySlack(String buildStatus = 'STARTED') {
+    // Build status of null means success.
+    buildStatus = buildStatus ?: 'SUCCESS'
+    def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n"
+    def color
 
-def call(String buildResult) {
-  if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
-  }
-  else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
-  }
-  else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
-  }
-  else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its result was unclear"	
-  }
-}
+    if (buildStatus == 'SUCCESS') {
+        color = '#00FF00'
+    } else if (buildStatus == 'UNSTABLE') {
+        color = '#ff0000'
+    } else {
+        color = '#ff0000'
+    }
+
+    if (buildStatus == 'STARTED') {
+        slackSend(channel: 'custom_pipelines_alerts', message: msg.concat("") , color: '#0000FF' )
+    } else if (buildStatus == 'SUCCESS') {
+        slackSend(channel: 'custom_pipelines_alerts', message: msg.concat("Successfully Updated To Desired  Count") , color: color )
+    } else if (buildStatus == 'UNSTABLE') {
+        slackSend(channel: 'custom_pipelines_alerts', message: msg.concat("Desired  Count Not Updated") , color: color )
+    } else {
+        slackSend(channel: 'custom_pipelines_alerts', message: msg.concat("Desired  Count Not Updated") , color: '#ff0000' )
+    }
+
+    }
