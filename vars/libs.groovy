fbae403d148b26281357607a77fd04e7aeb14f74
Shadab Shah <shahshadab1680@gmail.com>
2022-08-30 00:33:38 +0500

    updated

--- vars/libs.groovy
+++ vars/libs.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-def notifySlack(String buildStatus = 'STARTED') {
+def call(String buildStatus = 'STARTED') {
     // Build status of null means success.
     buildStatus = buildStatus ?: 'SUCCESS'
     def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n"
